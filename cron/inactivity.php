e368712ca0f91e58147180f02f36c815af5deba5
Dima2D3D <dima2d3d@gmail.com>
2026-09-09 14:44:55 +0300

    Restrict inactivity runner to CLI cron

--- cron/inactivity.php
+++ cron/inactivity.php
@@ -1,6 +1,9 @@
 <?php
+if (PHP_SAPI !== 'cli') {
+    http_response_code(403);
+    exit('CLI only');
+}
 require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../includes/inactivity.php';
-
 run_inactivity_reminders();
 echo "OK\n";
